04a0a8f2d5e888e9b8fdde78ae1e2cc8a795b4d7
Pathum Udayanga <udayangabandara900@gmail.com>
2025-03-25 13:25:32 +0530

    add repo

---
+++ AquaSphere Backend/AquaSphere-Backend/src/test/java/com/example/AquaSphere/Backend/AquaSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AquaSphere.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaSphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
