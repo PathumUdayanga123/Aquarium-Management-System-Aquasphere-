60778572a5dd540d56691f52b189ad0b17d900b2
chathum <deeparajchathum@gmail.com>
2025-05-08 23:43:15 +0530

    update

--- AquaSphere Backend/AquaSphere-Backend/src/main/java/com/example/AquaSphere/Backend/DTO/UserUpdateDTO.java
+++ AquaSphere Backend/AquaSphere-Backend/src/main/java/com/example/AquaSphere/Backend/DTO/UserUpdateDTO.java
@@ -9,7 +9,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserUpdateDTO {
     private String name;
+    private String email;
     private String address;
     private String postalCode;
     private String contactNo;
+
 }
